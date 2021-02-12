6d81f0f829b510325f2b1a28918c2db423a9fff3
meaningless777 <KazuhikoMiwa777@yahoo.co.jp>
2021-02-12 10:23:29 +0900

    my first commit

---
+++ pythonbook11002a.py
@@ -0,0 +1,42 @@
+def hangman ( word ) :
+    wrong = 0 
+    stages = [ '' , 
+               '__________      ' ,
+               '|               ' , 
+               '|       |       ' , 
+               '|       O       ' , 
+               '|      /|\      ' , 
+               '|      / \      ' , 
+               '|               ' , ]
+    rletters = list(word)
+    board= ["_"] * len(word)
+    win = False
+    print ( "ハングマンへようこそ！" )     
+#   for i in range(0,8) :
+#       print ( stages[i] ) 
+
+    while wrong < len(stages) - 1 :
+        print ( "\n" )
+        msg = "単語のうち、１文字を予想してね。"
+        char = input(msg)
+        if char in rletters :
+            print ( "ヒットしました。" ) 
+            cind = rletters.index(char)
+            board[cind] = char 
+            rletters[cind] = '$'
+        else :
+            print ( "ぶっぶー、はずれ。" ) 
+            wrong += 1 
+        print  ( " ".join( board ) )
+        e = wrong + 1
+        print ( "\n".join(stages[0:e]) )
+        if "_" not in board :
+            print ( "あなたの勝ち！おめでとうございます。正解は、" , end = '' )
+            print ( "".join(board) + "です。")
+            win = True 
+            break 
+    if not win : 
+        print ( "\n".join( stages[0:wrong+1] ) ) 
+        print ( "あなたは負けました！正解は{}でした。".format(word) )
+
+            
